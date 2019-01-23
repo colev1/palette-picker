26ef1e02e23c7f2d67bb164e67306ad60d53771d
Cole Vanacore <colevanacore@gmail.com>
2019-01-22 21:11:52 -0700

    Generate random hex

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,12 +1,20 @@
 var generateBtn = document.querySelector('.generate-btn')
 var savePaletteBtn = document.querySelector('.save-btn')
 var paletteInput = document.querySelector('.palette-name')
+var paletteOptions = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
 
 generateBtn.addEventListener('click', generateNewPalette)
 savePaletteBtn.addEventListener('click', savePalette)
 
 function generateNewPalette () {
-  console.log('new palette')
+  let randomHex = [];
+  for (var i=0; i< 6; i++) {
+    let randomVal = paletteOptions[Math.floor(Math.random() * paletteOptions.length)];
+    randomHex.push(randomVal);
+  }
+  let newHex = randomHex.join('').toUpperCase();
+  document.querySelector('.hex-1').innerHTML = `#${newHex}`;
+  document.querySelector('.circle').style.backgroundColor = `#${newHex}`;
 }
 
 function savePalette () {
