5845e6b983b1abb684205edacb796c7928e91d79
Cole Vanacore <colevanacore@gmail.com>
2019-01-22 21:25:26 -0700

    Random hex functionality

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,18 +3,35 @@ var savePaletteBtn = document.querySelector('.save-btn')
 var paletteInput = document.querySelector('.palette-name')
 var paletteOptions = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
 
-generateBtn.addEventListener('click', generateNewPalette)
+generateBtn.addEventListener('click', generateAllPalettes)
 savePaletteBtn.addEventListener('click', savePalette)
 
-function generateNewPalette () {
-  let randomHex = [];
-  for (var i=0; i< 6; i++) {
-    let randomVal = paletteOptions[Math.floor(Math.random() * paletteOptions.length)];
-    randomHex.push(randomVal);
+function generateAllPalettes() {
+  let palettesArray = [];
+  for(let i=0; i<5; i++) {
+    let newPalette = generateNewPalette();
+    palettesArray.push(newPalette)
+    console.log(palettesArray)
   }
-  let newHex = randomHex.join('').toUpperCase();
-  document.querySelector('.hex-1').innerHTML = `#${newHex}`;
-  document.querySelector('.circle').style.backgroundColor = `#${newHex}`;
+  let allHexes = document.querySelectorAll('.hex');
+  let allCircles = document.querySelectorAll('.circle');
+  for(let i=0; i<allHexes.length; i++) {
+    allHexes[i].innerHTML = `#${palettesArray[i]}`
+    allCircles[i].style.backgroundColor = `#${palettesArray[i]}`
+  }
+}
+
+function generateNewPalette () {
+    let randomHex = [];
+    let newHex;
+      for (let i=0; i< 6; i++) {
+      let randomVal = paletteOptions[Math.floor(Math.random() * paletteOptions.length)];
+      randomHex.push(randomVal);
+      newHex = randomHex.join('').toUpperCase();
+    }
+    return newHex
+  // document.querySelector('.hex-1').innerHTML = `#${newHex}`;
+  // document.querySelector('.circle').style.backgroundColor = `#${newHex}`;
 }
 
 function savePalette () {
