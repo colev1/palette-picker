f4eae75fb6dd6c09ecd468c598400826b321bf99
Cole Vanacore <colevanacore@gmail.com>
2019-01-25 09:45:27 -0700

    Knex

--- knexfile.js
+++ knexfile.js
@@ -8,16 +8,16 @@ module.exports = {
     migrations: {
       directory: './db/migrations'
     },
-    production: {
-      client: 'pg',
-      connection: process.env.DATABASE_URL + `?ssl=true`,
-      migrations: {
-        directory: './db/migrations'
-      },
-      useNullAsDefault: true
-    },
     seeds: {
       directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
     },
     useNullAsDefault: true
   }
