574ca99dbdad0cd0d757c426f85d635229e89159
Cole Vanacore <colevanacore@gmail.com>
2019-01-24 17:37:42 -0700

    Delete locals

--- server.js
+++ server.js
@@ -6,69 +6,9 @@ const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
 app.use( bodyParser.json() );
-
+app.use(express.static('public'));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
-app.locals.projects = [
-  {
-    id: 1,
-    name: 'Project 1'
-  },
-  {
-    id: 2,
-    name: 'Project 2'
-  },
-  {
-    id: 3,
-    name: 'Project 3'
-  }
-]
-
-app.locals.palettes = [
-  {
-    id: 1,
-    name: 'cool colors',
-    project_id: 1,
-    color_1: '#61E45C',
-    color_2: '#B6B2F0',
-    color_3: '#613D0E',
-    color_4: '#D67F0B',
-    color_5: '#D0D714'
-  },
-  {
-    id: 2,
-    name: 'dark colors',
-    project_id: 1,
-    color_1: '#61FFCF',
-    color_2: '#FF00F2',
-    color_3: '#ABC123',
-    color_4: '#11FFAA',
-    color_5: '#1F2A3A'
-  },
-  {
-    id: 3,
-    name: 'pastels',
-    project_id: 2,
-    color_1: '#61FFCF',
-    color_2: '#FF00FC',
-    color_3: '#ABC123',
-    color_4: '#11FFAA',
-    color_5: '#1F2A3A'
-  },
-  {
-    id: 4,
-    name: 'pastels',
-    project_id: 3,
-    color_1: '#61FFCF',
-    color_2: '#FF00FC',
-    color_3: '#ABC123',
-    color_4: '#11FFAA',
-    color_5: '#1F2A3A'
-  }
-]
-
-app.use(express.static('public'));
-
 
 //get all projects
 app.get('/api/v1/projects', (request, response) => {
@@ -150,7 +90,6 @@ app.post('/api/v1/projects/:id/palettes', (request, response) => {
       response.status(500).json({ error });
     });
 });
-
 
 //delete a palette from a project
 app.delete('/api/v1/projects/:id/palettes/:palette_id', (request, response) => {
